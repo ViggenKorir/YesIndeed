98591ae9a3aadc94e01c41668957c5405578fe57
Korir <koriremanuel0@gmail.com>
2025-10-23 14:56:57 +0300

    Added vercel analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import { type Metadata } from "next";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
 import AlertProvider from "@/components/ui/Alert";
+import { Analytics } from "@vercel/analytics/next";
 
 export const metadata: Metadata = {
   title: "YesIndeed",
@@ -20,6 +21,7 @@ export default function RootLayout({
           <AlertProvider>
             <header className="flex justify-end items-center p-4 gap-4 h-16" />
             <main>{children}</main>
+            <Analytics />
           </AlertProvider>
         </body>
       </html>
